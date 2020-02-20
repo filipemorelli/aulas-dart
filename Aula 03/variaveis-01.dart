8962d0301cd43a79f5fb6e5126f717fa9edf71b0
Filipe Morelli de Souza <morellitecinfo@gmail.com>
2020-02-20 18:44:13 +0000

    Aula 03 - Variaveis

---
+++ Aula 03/variaveis-01.dart
@@ -0,0 +1,18 @@
+void main() {
+  String name = "Joe";
+  bool isTall = true;
+  int age = 18;
+  double height = 1.82;
+  dynamic x = "a"
+  x = 1;
+  x = false;
+  List<Strings> animas = ["dog", "cat", "bird"];
+  Map<String, dynamic> = {
+    "btc": {
+      "price": 10000.00
+    },
+   "eth": {
+      "price": 8000.00
+    }
+  };
+}
---
+++ Aula 03/variaveis-02.dart
@@ -0,0 +1,4 @@
+/// O código resultará em um aviso, já que o valor atribuído à variável não corresponde ao tipo de dados da variável.
+void main() { 
+  String name = 1;
+}
